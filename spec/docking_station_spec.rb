bb268b37d0bc7af929f20abb673ba8fa48e85cfb
Carlos <chdezmar@gmail.com>
2016-05-24 15:03:12 +0100

    Added bike class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
 require "dockingStation"
+require "bike"
 
 describe DockingStation do
 it { is_expected.to respond_to :release_bike }
+it "Releases a bike" do
+  expect(subject.release_bike).to be_kind_of Bike
+end
 end
