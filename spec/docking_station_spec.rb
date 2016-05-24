013a43e3bbcf9429aea648f9a337e5593d4684aa
Carlos <chdezmar@gmail.com>
2016-05-24 16:12:49 +0100

    fix

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,7 @@ require "bike"
 describe DockingStation do
 it { is_expected.to respond_to :release_bike }
 it "Releases a bike" do
-  expect(subject.release_bike).to be_kind_of Bike
+  bike = subject.release_bike
+  expect(bike).to be_working
 end
 end
