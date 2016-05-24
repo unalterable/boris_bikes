ba7e13733675a88e56652dae92224ae3d8380f73
Hassan Radheyyan <hassanradheyyan@Hassans-MacBook-Pro.local>
2016-05-24 16:49:43 +0100

    Challenge 10 Completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
 	it {is_expected.to respond_to :release_bike}
+	it "returns bike & bike is working" do
+		bike = subject.release_bike
+		expect(bike.working?).to eq true
+	end
 end
