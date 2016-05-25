d64f493319c5305a6e94b15d211d6ea5d712c247
mbutlerw <mattb-w@hotmail.co.uk>
2016-05-25 12:16:16 +0100

    Initial commit Ophan day-two

---
+++ lib/docking_station.rb
@@ -0,0 +1,23 @@
+require_relative 'bike'
+
+class DockingStation
+	attr_reader :docked_bike
+
+	def release_bike
+		if @docked_bike == nil
+			raise "No bikes available."
+		else 
+		  Bike.new
+		end
+	end
+	
+	def dock(bike)
+		@docked_bike = bikee
+	end
+	
+	def docked
+		if @docked_bike != nil then return @docked_bike
+		else return false
+		end	
+	end
+end
