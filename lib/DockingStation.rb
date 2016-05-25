f48ee84571ed8389c798acf32ed4208549cbca99
Luke <lukeaveil@yahoo.com>
2016-05-25 18:12:50 +0100

    changed @bike to an empty array @bikes, changed methods and tests to allow multiple bikes

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,18 +1,22 @@
 require_relative 'bike'
 
 class DockingStation
-	attr_reader :bike
+	attr_reader :bikes
+
+	def initialize
+		@bikes = []
+	end
 
 	def release_bike
-		fail "There are no bikes" unless @bike
-		@bike
-
+		fail "There are no bikes" if @bikes.length == 0
+		@bikes.pop
+		
 	end
 
 	def dock (bike)
-		fail "This station is full" if @bike 
-				@bike = bike
-
+		fail "This station is full" if @bikes.length >= 20
+		@bikes << bike
+		bike
 	end
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -16,12 +16,12 @@ describe DockingStation do
 		it "releases a bike" do
 			bike = Bike.new
 			subject.dock(bike)
-			expect(subject.release_bike).to eq bike
+			expect(subject.release_bike).to be_working
 		end
 
 		it "raises an error when there are no bikes" do
-		expect {subject.release_bike}.to raise_error("There are no bikes")
-		end
+			expect {subject.release_bike}.to raise_error("There are no bikes")
+			end
 	end
 
 	it "responds to dock" do
@@ -36,7 +36,7 @@ describe DockingStation do
 			end
 
 		it "raises and error when the docking station is full" do
-			subject.dock(Bike.new)
+			20.times {subject.dock(Bike.new)}
 			expect {subject.dock(Bike.new)}.to raise_error("This station is full")
 			end
 	end
@@ -44,11 +44,11 @@ describe DockingStation do
 	it "returns a docked bike" do
 		bike = Bike.new
 		subject.dock(bike)
-		expect(subject.bike).to eq bike
+		expect(subject.bikes.last).to eq bike
 	end
 
 	it "responds to bike method" do
-		expect(subject).to respond_to :bike
+		expect(subject).to respond_to :bikes
 	end
 
 end
