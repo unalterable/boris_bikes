ee91f991123392227214b7a00b71ab248e97fc5d
Tim Chipperfield <timchipperfield@Tims-MacBook-Pro.local>
2016-05-25 14:48:39 +0100

    added bike method and tests for bike and dock

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,11 +1,14 @@
 require_relative 'bike'
 
 class DockingStation
+	attr_reader :bike
+
 	def release_bike
 		Bike.new
 	end
 
-	def dock
-
+	def dock (bike)
+		@bike = bike
 	end
+
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -11,9 +11,21 @@ describe DockingStation do
 		expect(bike).to be_working
 	end
 	it "responds to dock" do
-		expect(subject).to respond_to :dock
-	end 
-	it "excepts a bike" do
-		expect(subject).to receive(bike)
+		expect(subject).to respond_to(:dock).with(1).argument
+	end
+
+	it "docks a bike" do
+		bike = Bike.new
+		expect(subject.dock(bike)).to eq bike
+	end
+
+	it "returns a docked bike" do
+		bike = Bike.new
+		subject.dock(bike)
+		expect(subject.bike).to eq bike
+	end
+
+	it "responds to bike method" do
+		expect(subject).to respond_to :bike
 	end
 end
