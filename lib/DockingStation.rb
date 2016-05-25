cfc011a445cf08368936f5428a449f799e1a2f27
Luke <lukeaveil@yahoo.com>
2016-05-25 15:49:03 +0100

    tests written for rleases bike and error message, release bike method updated with error exception

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -4,7 +4,9 @@ class DockingStation
 	attr_reader :bike
 
 	def release_bike
-		Bike.new
+		fail "There are no bikes" unless @bike
+		@bike
+
 	end
 
 	def dock (bike)
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -6,10 +6,24 @@ describe DockingStation do
 	it "responds to release_bike" do
 		expect(subject).to respond_to :release_bike
 	end
-	it "releases the working bike" do
-		bike = subject.release_bike
-		expect(bike).to be_working
+
+	describe "#release_bike" do
+		#it "releases the working bike" do
+			#bike = subject.release_bike
+			#expect(bike).to be_working
+		#end
+
+		it "releases a bike" do
+			bike = Bike.new
+			subject.dock(bike)
+			expect(subject.release_bike).to eq bike
+		end
+
+		it "raises an error when there are no bikes" do
+		expect {subject.release_bike}.to raise_error("There are no bikes")
+		end
 	end
+
 	it "responds to dock" do
 		expect(subject).to respond_to(:dock).with(1).argument
 	end
@@ -28,4 +42,5 @@ describe DockingStation do
 	it "responds to bike method" do
 		expect(subject).to respond_to :bike
 	end
+
 end
