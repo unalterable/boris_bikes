8eeb3a635597511f6e2c68159602a6bca22ae1ce
mbutlerw <mattb-w@hotmail.co.uk>
2016-05-25 13:08:01 +0100

    Fixed Test cases to raise/expect error

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,20 +4,17 @@ class DockingStation
 	attr_reader :docked_bike
 
 	def release_bike
-		if @docked_bike == nil
-			raise "No bikes available."
-		else 
-		  Bike.new
-		end
+		fail 'No bikes available' unless @docked_bike
+		@docked_bike
 	end
-	
+
 	def dock(bike)
-		@docked_bike = bikee
+		@docked_bike = bike
 	end
-	
-	def docked
+
+	def docked?
 		if @docked_bike != nil then return @docked_bike
 		else return false
-		end	
+		end
 	end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,17 +3,19 @@ require 'docking_station'
 describe DockingStation do
 	it {is_expected.to respond_to :release_bike}
 	it "returns bike & bike is working" do
+		bike1 = Bike.new
+		subject.dock(bike1)
 		bike = subject.release_bike
 		expect(bike.working?).to eq true
 	end
 	it { is_expected.to respond_to(:dock).with(1).argument }
-	it { is_expected.to respond_to(:docked)}
+	it { is_expected.to respond_to(:docked?)}
 	it "returns true if bike is docked" do
-		bike = subject.release_bike
+		bike = Bike.new
 		subject.dock(bike)
-		expect(subject.docked).to eq bike
+		expect(subject.docked?).to eq bike
 	end
 	it "returns an error if no bikes are left." do
-		expect {subject.release_bike}.to raise_error("No bikes available.")
+		expect {subject.release_bike}.to raise_error("No bikes available")
 	end
 end
