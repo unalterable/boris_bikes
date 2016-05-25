eda5df0f709b8064df0aba2d888e258c22833e1f
Toby Wilkins <tobywilkins@Tobys-MacBook-Pro-2.local>
2016-05-25 15:26:01 +0100

    Added changable capacity to docking station & made code more DRY & secure

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,24 +1,32 @@
 class DockingStation
-  attr_reader :bikes
-  def initialize
+  DEFAULT_CAPACITY = 20
+  attr_reader :capacity
+  def initialize capacity = DEFAULT_CAPACITY
     @bikes = []
+    @capacity = capacity
   end
 
-  def release_bikes
+  def release_bike
       raise "Bike not available" if empty?
-      @bikes.pop
+      bikes.pop
   end
 
   def dock(bike)
     raise("Docking capacity reached") if full?
-    @bikes << bike
+    bikes << bike
   end
 
+private
+
+  attr_reader :bikes
+
   def empty?
-    @bikes.count <= 0
+    bikes.count <= 0
   end
 
   def full?
-    @bikes.count >= 20
+    bikes.count >= @capacity
   end
 end
+
+
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -9,24 +9,16 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:release_bike) }
 
-  it { is_expected.to respond_to(:bikess) }
-
   it { is_expected.to respond_to(:dock).with(1).argument }
-
-  it 'returns empty array if no bike has been docked' do
-    expect(subject.bikes).to eq([])
-  end
 
   it 'should return bike if one is docked' do
     subject.dock("bike3")
-    expect(subject.bikes.count).to eq 1
     expect(subject.release_bike).to eq("bike3")
   end
 
   it 'returns the bike if bike has been docked' do
   subject.dock("bike3")
-  expect(subject.bikes.count).to eq 1
-  expect(subject.bikes).to eq(["bike3"])
+  expect(subject.release_bike).to eq("bike3")
   end
 
   it 'should not give out infinite bikes' do
@@ -34,7 +26,15 @@ describe DockingStation do
   end
 
   it 'should not allow docking if full (capacity = 20)' do
-    20.times {subject.dock("another bike")}
+    subject.class::DEFAULT_CAPACITY.times {subject.dock("another bike")}
     expect {subject.dock("Bike4")}.to raise_error("Docking capacity reached")
   end
+
+    it 'should allow specified capacities' do
+    dock_a = DockingStation.new(40)
+    20.times {dock_a.dock("another bike")}
+    expect(dock_a.release_bike).to eq("another bike")
+    21.times {dock_a.dock("another bike")}
+    expect {dock_a.dock("Bike4")}.to raise_error("Docking capacity reached")
   end
+end
