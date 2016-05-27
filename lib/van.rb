0e60539ed7c72c53361e43738eb1b63f5d30741c
Oliver Diamond <oliverdiamond@Olivers-MacBook-Pro-5.local>
2016-05-27 18:04:58 +0100

    Added a van class and van spec

--- lib/van.rb
+++ lib/van.rb
@@ -1,9 +1,18 @@
 class Van
 
+	attr_accessor :cargo
 	def initialize 
+		@cargo = []
 	end
 
 	def accept_bikes(array_of_bikes)
+		cargo.concat(array_of_bikes)
 	end
+
+	def unload_bikes
+		cargo
+	end
+
+	private
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -7,10 +7,28 @@ describe Van do
 		it "has the method" do
 			expect(subject).to respond_to(:accept_bikes).with(1).argument
 		end
+		it "actually has the bikes" do
+			array_of_bikes = [double(:bike),double(:bike),double(:bike),double(:bike)]
+			subject.accept_bikes(array_of_bikes)
+			expect(subject.instance_variable_get(:@cargo)).to eq(array_of_bikes)
+		end
 
-		
 	end
 
+	describe "#unload_bikes" do
+		it "has the method" do
+			expect(subject).to respond_to(:unload_bikes)
+		end
+		it "returns an array" do
+			expect(subject.unload_bikes).to be_a(Array)
+		end	
+		# it "returns an array of loaded bikes" do
+		# 	array_of_bikes = [double(:bike),double(:bike),double(:bike),double(:bike)]
+		# 	subject.accept_bikes(array_of_bikes)
+		# 	expect(subject.unload_bikes).to eq(array_of_bikes)
+		# end
+
+	end
 
 
 end
